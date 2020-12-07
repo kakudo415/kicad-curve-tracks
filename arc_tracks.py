1ee04a15f1313a3743ba1b3c299e231c32fab736
kakudo <kakudo415@gmail.com>
2020-12-07 17:25:14 +0900

    + category

--- arc_tracks.py
+++ arc_tracks.py
@@ -3,10 +3,11 @@ import os
 
 class ArcTracks(pcbnew.ActionPlugin):
     def defaults(self):
-        self.name = "Arc Tracks"
-        self.description = "Filling the gaps between tracks with arc"
+        self.name                = "Arc Tracks"
+        self.category            = "Wiring"
+        self.description         = "Filling the gaps between tracks with arc"
         self.show_toolbar_button = True
-        self.icon_file_name = os.path.join(os.path.dirname(__file__), "icon.png")
+        self.icon_file_name      = os.path.join(os.path.dirname(__file__), "icon.png")
     
     def Run(self):
         print("hoge")
